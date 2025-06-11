adb891c5d5a1f3e441922978a8c0c8a97ab0ba9d
kadirsener1 <119889025+kadirsener1@users.noreply.github.com>
2025-06-11 21:13:40 +0300

    Update sentv.py

--- sentv.py
+++ sentv.py
@@ -5,7 +5,8 @@ m3u_urls = [
     "https://raw.githubusercontent.com/kadirsener1/CanliTvListe/refs/heads/main/yeni.m3u",
     "https://raw.githubusercontent.com/kadirsener1/CanliTvListe/refs/heads/main/rectv.m3u",
     "https://raw.githubusercontent.com/kadirsener1/CanliTvListe/refs/heads/main/trgoals.m3u",
-    "https://raw.githubusercontent.com/kadirsener1/CanliTvListe/refs/heads/main/daddylive.m3u"
+    "https://raw.githubusercontent.com/kadirsener1/CanliTvListe/refs/heads/main/daddylive.m3u",
+    "https://raw.githubusercontent.com/kadirsener1/CanliTvListe/refs/heads/main/vavoo.m3u"
 ]
 
 # Çıktı dosyası adı
