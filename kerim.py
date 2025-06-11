373ae38f9107730731ceb400b6e3fbaab270646d
kadirsener1 <119889025+kadirsener1@users.noreply.github.com>
2025-06-11 12:56:30 +0300

    Update kerim.py

--- kerim.py
+++ kerim.py
@@ -2,6 +2,7 @@ import re
 import os
 
 KANAL_ADLARI = {
+    "Tv 8,5": "1901",
     "Szc Tv": "3002",
     "Sinema Tv": "1908",
     "FX": "313131",
@@ -38,8 +39,8 @@ KANAL_ADLARI = {
     "Eurosport 1": "2002",
     "Eurosport 2": "1809",
     "S Sport": "1810",
-    "S Sport 2": "3901",
-    "Tv 8,5": "1901"
+    "S Sport 2": "3901"
+    
 }
 
 def parse_m3u(file_path):
