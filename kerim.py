21aa285d843a389b48b0d1ea9aa113a0bf46bcfe
kadirsener1 <119889025+kadirsener1@users.noreply.github.com>
2025-06-12 09:26:00 +0300

    Update kerim.py

--- kerim.py
+++ kerim.py
@@ -38,7 +38,8 @@ KANAL_ADLARI = {
     "Eurosport 1": "2002",
     "Eurosport 2": "1809",
     "S Sport": "1810",
-    "S Sport 2": "3901"
+    "S Sport 2": "3901",
+    "Epic Drama": "2802"
     
 }
 
